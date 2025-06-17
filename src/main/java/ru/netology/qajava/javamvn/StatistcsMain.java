d17bd7a580c05577aa1d2618b409eb81efd9cb98
Denis <deniskazarinov@gmail.com>
2025-06-17 12:10:10 +0300

    add JoCoCo

---
+++ src/main/java/ru/netology/qajava/javamvn/StatistcsMain.java
@@ -0,0 +1,13 @@
+package ru.netology.qajava.javamvn;
+
+class StatisticsService {
+    public long findMax(long[] incomes) {
+        long currentMax = incomes[0];
+        for (long income : incomes) {
+            if (currentMax < income) {
+                currentMax = income;
+            }
+        }
+        return currentMax;
+    }
+}
